62ec543261ef7ef3a1b370d161cf0be86eb363dc
Toivanen-Gripentrog Tuuli <tgtuuli@cubbli18-gold.local>
2020-09-06 19:11:00 +0300

    Hahmoteltu solmun ominaisuuksia. Kaipaa vielä muutoksia

--- tiralabra-markov-musageneraattori/src/main/java/markovgeneraattori/TrieSolmu.java
+++ tiralabra-markov-musageneraattori/src/main/java/markovgeneraattori/TrieSolmu.java
@@ -8,17 +8,37 @@ import java.util.HashMap;
  */
 public class TrieSolmu {
     
+    private byte tunnus;
     private HashMap<Byte, TrieSolmu> lapset;
-    private boolean onSananLoppu;
+    public int laskuri;
 
     public TrieSolmu() {
         this.lapset = new HashMap<>();
-        this.onSananLoppu = false;
+        laskuri = 1;
     }
+    public TrieSolmu( byte tunnus) {
+        this.lapset = new HashMap<>();
+        laskuri = 1;
+        this.tunnus = tunnus;
+    }
+    
 
     public HashMap<Byte, TrieSolmu> getLapset() {
         return lapset;
     }
+    
+    public int getLaskuri(){
+        return laskuri;
+    }
+    
+    public void lisaaLaskuriin(){
+        laskuri += 1;
+    }
+
+    public byte getTunnus() {
+        return tunnus;
+    }
+    
     
     
      
