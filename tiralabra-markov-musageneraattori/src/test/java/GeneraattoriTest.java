3dce074bb91af355e6fbadee086f68bdde0c7595
Toivanen-Gripentrog Tuuli <tgtuuli@cubbli18-gold.local>
2020-10-02 22:42:16 +0300

    Lisää testejä sekä siivousta

--- tiralabra-markov-musageneraattori/src/test/java/GeneraattoriTest.java
+++ tiralabra-markov-musageneraattori/src/test/java/GeneraattoriTest.java
@@ -17,36 +17,33 @@ import org.junit.Before;
  */
 public class GeneraattoriTest {
     private Generaattori g;
-    @Before
-    public void alustus() {
+    private Trie t;
+    private Taulukkolista<Byte> lista;
+
+    public GeneraattoriTest() {
         this.g = new Generaattori(3, 4, 4, 8);
+        this.t = new Trie(3);
+        this.lista = new Taulukkolista();
+        Byte[] alkiot = {2,2,5,4,7,6,6,1,2,5,6,1,2,3,8,9,6,4,5,3,7,7,5,4,4,3,3,1};
+        lista.lisaaMonta(alkiot);
+        t.lisaa(lista);
     }
     
     @Test
     public void muodostaaOikeanPituisenSekvenssin() {
-        Trie t = new Trie(3);
-        Taulukkolista<Byte> lista = new Taulukkolista();
-        Byte[] alkiot = {2,2,5,4,7,6,6,1,2,5,6,1,2,3,8,9,6,4,5,3,7,7,5,4,4,3,3,1};
-        lista.lisaaMonta(alkiot);
-        t.lisaa(lista);
-        byte[] sekvenssi = g.muodostaSekvenssi(20, t, (byte) 4);
+        
+        byte[] sekvenssi = g.muodostaSekvenssi(20, t, (byte) 4, 3);
         
         assertEquals(20, sekvenssi.length);
         
     }
     @Test
     public void sekvenssiAlkaaOikeallaAlkiolla() {
-        Trie t = new Trie(3);
-        Byte[] alkiot = {2,2,5,4,7,6,6,1,2,5,6,1,2,3,8,9,6,4,5,3,7,7,5,4,4,3,3,1};
-        Taulukkolista<Byte> lista = new Taulukkolista();
-        lista.lisaaMonta(alkiot);
-        t.lisaa(lista);
-        byte[] sekvenssi = g.muodostaSekvenssi(20, t, (byte) 4);
         
+        byte[] sekvenssi = g.muodostaSekvenssi(20, t, (byte) 4, 3);
         assertEquals(4, sekvenssi[0]);
         
     }
-    
     
     
 }
--- tiralabra-markov-musageneraattori/src/test/java/RytmiGeneraattoriTest.java
+++ tiralabra-markov-musageneraattori/src/test/java/RytmiGeneraattoriTest.java
@@ -4,14 +4,38 @@
  * and open the template in the editor.
  */
 
+import markovgeneraattori.generaattori.RytmiGeneraattori;
+import markovgeneraattori.tietorakenteet.Taulukkolista;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author tgtuuli
  */
 public class RytmiGeneraattoriTest {
+    
+    private Taulukkolista<Byte> rytmit;
+    private RytmiGeneraattori rytmiGeneraattori;
+
+    
+    @Before
+    public void alusta() {
+        Byte[] rytmi = {4,4,4,2,2,1,4,8,8,8,8,4,4,6,4,2,2,1};
+        rytmit = new Taulukkolista<>();
+        rytmit.lisaaMonta(rytmi);
+        rytmiGeneraattori = new RytmiGeneraattori(3, 4, 4, 12);
+    }
+    
+    
+    @Test
+    public void rytmiAlkaaOikeallaRytmilla() {
+        rytmiGeneraattori.generoiRytmi((byte) 4);
+        Taulukkolista<Byte> muodostettuRytmi = rytmiGeneraattori.getMuodostettuRytmi();
+        
+        assertEquals((byte) 4, (byte) muodostettuRytmi.get(0));
+    }
     
    
 }
